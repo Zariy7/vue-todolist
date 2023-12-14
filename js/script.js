6a2689d2022f9e58b8f76b25a1f69c1dd04d3ef0
Zariy7 <nazariy.hereha@gmail.com>
2023-12-14 08:42:22 +0100

    milestone 1 complete

--- js/script.js
+++ js/script.js
@@ -2,9 +2,30 @@ let { createApp } = Vue;
 
 createApp({
     data() {
-      return {
-        
-      }
+        return {
+            todoList: [
+                {
+                    text: 'Walk the dog.',
+                    done: true,
+                },
+                {
+                    text: 'Buy the groceries.',
+                    done: false,
+                },
+                {
+                    text: 'Fill the car.',
+                    done: true,
+                },
+                {
+                    text: 'Pay the bills.',
+                    done: false,
+                },
+                {
+                    text: 'Wash the clothes.',
+                    done: false,
+                }
+            ]
+        }
     },
     methods: {
         
